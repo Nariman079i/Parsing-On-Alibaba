4cd56d1ac3aacc84ea353e6f19cbb59e8a59fc88
Nariman079i <maga_mag_2019@mail.ru>
2022-01-27 13:20:24 +0300

    unfiled

--- main.py
+++ main.py
@@ -4,23 +4,39 @@ from requests import *
 from bs4 import BeautifulSoup as Bs
 from time import time
 
-url = "https://www.alibaba.com/trade/search?IndexArea=product_en&SearchText=iphone12&page=2"
+_url = "https://www.alibaba.com/trade/search?IndexArea=product_en&SearchText=iphone12&page=2"
 _parser = "lxml"
 
 def search_goods():
 
-    content = get(url)
-    print(content.status_code)
-    content = content.text
+    content = get(_url)
+
+    if content.status_code == 200:
+
+        text = content.text
+        soup = Bs(text , _parser)
+        title = soup.findAll('h2')
+
+        urls = []
+
+        for t in title:
+            urls.append("https:"+str(t.find('a').get("href")))
+
+        for url in urls[0:2]:
+            content = get(url).text
+            soup = Bs(content , _parser).find_all('div')
+
+            for elem in soup:
+                r = str(elem.find('h1' ))
+                if r == "None":
+                    continue
+                else:
+                    print(r.split('"')[3].split("  ")[0])
 
 
-    soup = Bs(content , _parser)
-    div = soup.findAll('h2')
 
 
-    for i in div:
-        outPut = str(i.find('a').get("href"))
-        print(outPut.strip('//'))
+
 
 
 search_goods()
