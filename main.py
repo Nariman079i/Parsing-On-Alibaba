85ff624e136a68ad9e5b38ffbc694fd0da0a86d5
Nariman079i <maga_mag_2019@mail.ru>
2022-01-27 11:36:28 +0300

    fix bugs

--- main.py
+++ main.py
@@ -4,16 +4,23 @@ from requests import *
 from bs4 import BeautifulSoup as Bs
 from time import time
 
-url = "https://www.alibaba.com/trade/search?IndexArea=product_en&SearchText=iphone12&page=2&f0=y"
-_parser = "html.parser"
+url = "https://www.alibaba.com/trade/search?IndexArea=product_en&SearchText=iphone12&page=2"
+_parser = "lxml"
 
 def search_goods():
 
     content = get(url)
+    print(content.status_code)
     content = content.text
 
-    soup = Bs(content , _parser)
 
-    print(soup)
+    soup = Bs(content , _parser)
+    div = soup.find_all('h2')
+    price = soup.find_all('div')
+
+    for i in price:
+
+        print(i.find('div', _class="list-no-v2-outter J-offer-wrapper").find('span'))
+
 
 search_goods()
