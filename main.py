d4ea4fba595bdbd3a9115165ebcd843da60ebc70
Nariman079i <maga_mag_2019@mail.ru>
2022-01-27 11:13:41 +0300

    create def for search goods

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 #парсинг товаров с Alibaba
+
+from requests import *
+from bs4 import BeautifulSoup as Bs
+from time import time
+
+url = "https://www.alibaba.com/trade/search?IndexArea=product_en&SearchText=iphone12&page=2&f0=y"
+_parser = "html.parser"
+
+def search_goods():
+
+    content = get(url)
+    content = content.text
+
+    soup = Bs(content , _parser)
+
+    print(soup)
+
+search_goods()
